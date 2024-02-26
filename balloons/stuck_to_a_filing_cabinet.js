0172bb9435fe72cdd8db5bf31dc2194ac1c8360c
Zichqec <36972872+Zichqec@users.noreply.github.com>
2024-02-26 13:24:09 -0500

    The rest of the new pages
    Plus some alt text I missed and some link fixes

---
+++ balloons/stuck_to_a_filing_cabinet.js
@@ -0,0 +1,30 @@
+let reviewurl = "https://docs.google.com/forms/d/e/1FAIpQLSex4P04NnrezQOdr1MaTqQhxzoJ-umnk1bXq7nZEgURsvH70A/viewform?usp=sf_link";
+
+let latest_version = "";
+if (typeof balloon_downloads != "undefined") //Check if we're on a page that needs this info, to avoid an error
+{
+	latest_version = balloon_downloads["stuck_to_a_filing_cabinet"]
+	
+	if (document.getElementById('download'))
+		document.getElementById('download').innerHTML = `<a href="${latest_version}" class="dl_button">Download latest version</a>`;
+}
+
+
+let reviews = [
+	
+	{
+		review: `a balloon of balloons! so many balloons my head might explode!!! :exploding_head:`,
+		attribution: "Zichqec, October 15, 2022"
+	}
+	
+]
+
+let version_history = [
+	
+	{
+		version: "Download Balloons Balloon here!",
+		download: latest_version,
+		comment: "The most recent update can be found on Zichqec's site!"
+	}
+	
+]
---
+++ shells/correctly_drawn_flamingo.js
@@ -0,0 +1,30 @@
+let reviewurl = "https://docs.google.com/forms/d/e/1FAIpQLScfxRcYnRrro_8pcvKp9ncUAbUNiFTMy8KyVYEwt-E5xXjF2g/viewform?usp=sf_link";
+
+let latest_version = "";
+if (typeof shell_downloads != "undefined") //Check if we're on a page that needs this info, to avoid an error
+{
+	latest_version = shell_downloads["correctly_drawn_flamingo"]
+	
+	if (document.getElementById('download'))
+		document.getElementById('download').innerHTML = `<a href="${latest_version}" class="dl_button">Download latest version</a>`;
+}
+
+
+let reviews = [
+	
+	{
+		review: `roundly pointy`,
+		attribution: "Claw Grasshammer, April 9, 2023"
+	}
+	
+]
+
+let version_history = [
+	
+	{
+		version: "v1.0.0",
+		download: latest_version,
+		comment: "Initial Release."
+	}
+	
+]
