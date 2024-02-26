44c24f968525e03c67198719e0cdc8f68052c416
Zichqec <36972872+Zichqec@users.noreply.github.com>
2024-02-26 00:06:49 -0500

    Add alt text to banners & new ghost pages

---
+++ ghosts/gallery_512.js
@@ -0,0 +1,30 @@
+let reviewurl = "https://forms.gle/TScJferrfkwaQHwn7";
+
+let latest_version = "";
+if (typeof ghost_downloads != "undefined") //Check if we're on a page that needs this info, to avoid an error
+{
+	latest_version = ghost_downloads["balloons"]
+	
+	if (document.getElementById('download'))
+		document.getElementById('download').innerHTML = `<a href="${latest_version}" class="dl_button">Download latest version</a>`;
+}
+
+
+let reviews = [
+	
+	{
+		review: ``,
+		attribution: ""
+	}
+	
+]
+
+let version_history = [
+
+	{
+		version: "Download Balloons here!",
+		download: latest_version,
+		comment: "The most recent update can be found on Zichqec's site!",
+	}
+	
+]
